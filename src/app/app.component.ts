59ba11eb188a62cf979d72851cb2bbb0e9125c6a
Place4you <sadiraja500@gmail.com>
2024-09-01 11:30:06 +0500

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from "./Components/header/header.component";
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HeroComponent } from './Components/hero/hero.component';
 import { AboutComponent } from './Components/about/about.component';
-import { ServiceComponent } from './componenet/service/service.component';
+import { ServiceComponent } from './Components/service/service.component';
+import { CompaniesComponent } from './componenet/companies/companies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ServiceComponent , HeaderComponent, NavbarComponent, HeroComponent,AboutComponent],
+  imports: [RouterOutlet,ServiceComponent , CompaniesComponent , HeaderComponent, NavbarComponent, HeroComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
