cebbc919890f7b7c4e7b6bcb568463da06231dea
Place4you <sadiraja500@gmail.com>
2024-09-01 13:03:40 +0500

    new

--- src/app/Components/companies/companies.component.ts
+++ src/app/Components/companies/companies.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-companies',
   standalone: true,
-  imports: [],
+  imports: [ ],
   templateUrl: './companies.component.html',
   styleUrl: './companies.component.css'
 })
